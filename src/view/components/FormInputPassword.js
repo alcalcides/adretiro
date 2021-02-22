5849105a12be319c9e3eab12dfc3517567e29285
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2021-02-21 23:27:58 -0300

    Allows to users see the password typed

--- src/view/components/FormInputPassword.js
+++ src/view/components/FormInputPassword.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaEyeSlash, FaEye } from "react-icons/fa";
 
 export default function FormInputPassword({
   password,
@@ -7,12 +8,25 @@ export default function FormInputPassword({
   propertyID,
   tip = "Use de 8 a 25 caracteres alfanuméricos. Banco de dados seguro: todas as senhas são criptografadas",
 }) {
+  const [visible, setVisible] = useState(false);
+
+  function togleVisibility() {
+    setVisible(!visible);
+    if (document.getElementById(propertyID).type === "password") {
+      document.getElementById(propertyID).type = "text";
+    } else {
+      document.getElementById(propertyID).type = "password";
+    }
+  }
+
   return (
-    <div className="form-group">
-      <label htmlFor={propertyID}>{labelContent}</label>
+    <div className="form-group row no-gutters">
+      <label htmlFor={propertyID} className="col-12">
+        {labelContent}
+      </label>
       <input
         type="password"
-        className="form-control"
+        className="form-control col-10"
         id={propertyID}
         value={password}
         onChange={(e) => setPassword(e.target.value)}
@@ -20,6 +34,10 @@ export default function FormInputPassword({
         maxLength="25"
         required
       />
+      
+      {!visible && <FaEye className="col-2" onClick={togleVisibility} />}
+      {visible && <FaEyeSlash className="col-2" onClick={togleVisibility} />}
+
       <small id={`${propertyID}Help`} className="form-text text-muted h5">
         {tip}
       </small>
