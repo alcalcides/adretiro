30722d73f177e83744c73fa7f51fef9aac1af8a2
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2021-03-07 23:33:11 -0300

    Removes lan dns configuration

--- src/model/services/interface/getServer.js
+++ src/model/services/interface/getServer.js
@@ -1,11 +1,10 @@
 import { servers } from "../../servers.json";
 
 export function getServer() {
-  let server = servers.blind;
-  
+  let server = "google.com";
+
   const dns = window.location.hostname;
   if (dns === "localhost") server = servers.development;
-  else if (dns === "10.0.0.117") server = servers.lan;
   else if (dns === "adretiro-dev.herokuapp.com") server = servers.review;
   else if (dns === "adretiro.com.br") server = servers.production;
 
