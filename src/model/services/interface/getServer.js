920439c2c4dd2cf62d0f34100a3fc95d01ca1489
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2021-03-12 22:04:23 -0300

    Cleans the code getServers

--- src/model/services/interface/getServer.js
+++ src/model/services/interface/getServer.js
@@ -1,12 +1,23 @@
 import { servers } from "../../servers.json";
 
 export function getServer() {
-  let server = "google.com";
+  let server;
 
   const dns = window.location.hostname;
-  if (dns === "localhost") server = servers.development;
-  else if (dns === "adretiro-dev.herokuapp.com") server = servers.review;
-  else if (dns === "adretiro.com.br") server = servers.production;
+  switch (dns) {
+    case "localhost":
+      server = servers.development;
+      break;
+    case "adretiro-dev.herokuapp.com":
+      server = servers.review;
+      break;
+    case "adretiro.com.br":
+      server = servers.production;
+      break;  
+    default:
+      server = servers.trap;
+      break;
+  }
 
   return server;
 }
